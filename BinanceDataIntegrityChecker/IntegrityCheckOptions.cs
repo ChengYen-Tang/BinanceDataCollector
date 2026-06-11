d8918cbf0923872cee4021acf53f37fa601b694f
Kenneth Tang <Kenneth@nssh.onmicrosoft.com>
2026-06-11 23:59:40 +0800

    Parallelize DuckDB integrity checks

--- BinanceDataIntegrityChecker/IntegrityCheckOptions.cs
+++ BinanceDataIntegrityChecker/IntegrityCheckOptions.cs
@@ -8,6 +8,8 @@ public sealed class IntegrityCheckOptions
 
     public int MaxMissingDaysToLog { get; set; } = 20;
 
+    public int MaxDegreeOfParallelism { get; set; } = Math.Max(1, Environment.ProcessorCount);
+
     public DataTypeOptions DataTypes { get; set; } = new();
 
     public sealed class DataTypeOptions
